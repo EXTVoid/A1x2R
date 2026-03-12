1946f88dc1409b8fee58d30fa770d50370435176
LENAR <EXTVoid@users.noreply.github.com>
2026-03-12 10:17:23 +0800

    Update

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -11,11 +11,12 @@ src-git small https://github.com/kenzok8/small
 # Extra packages
 src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite
 src-git lite_watchdog https://github.com/4IceG/luci-app-lite-watchdog
-src-git my_pkg https://github.com/0x4C334E3438/My_PKG
+src-git my_pkg https://github.com/EXTVoid/My_PKG
 src-git modemband https://github.com/4IceG/luci-app-modemband
 src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
 src-git sms_tool_js https://github.com/4IceG/luci-app-sms-tool-js
 src-git xray https://github.com/yichya/luci-app-xray
 src-git v2raya https://github.com/v2rayA/v2raya-openwrt
 src-git zapret https://github.com/remittor/zapret-openwrt;zap1
+src-git zapret2 https://github.com/remittor/zapret-openwrt;master
 EOF
