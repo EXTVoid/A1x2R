4bc272494210c49a3ef1b650506e8a61c2644fcd
EXTVoid <175212606+EXTVoid@users.noreply.github.com>
2026-05-18 10:10:56 +0800

    Update feeds.sh

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -5,18 +5,19 @@ sed -Ei 's|git.openwrt.org/(feed\|project)|github.com/openwrt|g' ./feeds.conf.de
 
 cat >> ./feeds.conf.default <<EOF
 # Extra feeds
-src-git openwrt_packages https://github.com/kenzok8/openwrt-packages
-src-git small https://github.com/kenzok8/small
+# src-git openwrt_packages https://github.com/kenzok8/openwrt-packages
+# src-git small https://github.com/kenzok8/small
+src-git small_package https://github.com/kenzok8/small-package
 
 # Extra packages
-src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite
-src-git lite_watchdog https://github.com/4IceG/luci-app-lite-watchdog
-src-git my_pkg https://github.com/EXTVoid/My_PKG
+src-git luci_app_3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite
+src-git luci_app_lite_watchdog https://github.com/4IceG/luci-app-lite-watchdog
+src-git luci_app_modemband https://github.com/4IceG/luci-app-modemband
+src-git luci_app_sms_tool_js https://github.com/4IceG/luci-app-sms-tool-js
+src-git luci_app_xray https://github.com/yichya/luci-app-xray
 src-git modemband https://github.com/obsy/modemband
-src-git modemband_luci https://github.com/4IceG/luci-app-modemband
-src-git sms_tool_js https://github.com/4IceG/luci-app-sms-tool-js
-src-git xray https://github.com/yichya/luci-app-xray
-src-git v2raya https://github.com/v2rayA/v2raya-openwrt
+src-git my_pkg https://github.com/EXTVoid/My_PKG
+src-git v2raya_openwrt https://github.com/v2rayA/v2raya-openwrt
 src-git zapret https://github.com/remittor/zapret-openwrt;zap1
 src-git zapret2 https://github.com/EXTVoid/zapret-openwrt;master
 EOF
