cfef13c64ab122f304234dc3ece7372774b6cba6
EXTVoid <175212606+EXTVoid@users.noreply.github.com>
2026-05-18 09:45:22 +0800

    Update feeds.sh

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -12,8 +12,8 @@ src-git small https://github.com/kenzok8/small
 src-git 3ginfo_lite https://github.com/4IceG/luci-app-3ginfo-lite
 src-git lite_watchdog https://github.com/4IceG/luci-app-lite-watchdog
 src-git my_pkg https://github.com/EXTVoid/My_PKG
-src-git modemband https://github.com/4IceG/luci-app-modemband
-src-git modemdefine https://github.com/4IceG/luci-app-modemdefine
+src-git modemband https://github.com/obsy/modemband
+src-git modemband_luci https://github.com/4IceG/luci-app-modemband
 src-git sms_tool_js https://github.com/4IceG/luci-app-sms-tool-js
 src-git xray https://github.com/yichya/luci-app-xray
 src-git v2raya https://github.com/v2rayA/v2raya-openwrt
